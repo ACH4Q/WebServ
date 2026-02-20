73f2554daac6812999a55f01f2ee0b3ab44ddac6
Achaq Mohammed Amine <138736773+ACH4Q@users.noreply.github.com>
2026-02-20 17:57:19 +0000

    adding enum for easy parsg

--- includes/HttpReq.hpp
+++ includes/HttpReq.hpp
@@ -6,15 +6,33 @@
 #include <string>
 #include <map>
 
-class HttpReq
+enum RequestParseState
+{
+    Request_Line,          // GET /images/logo.png HTTP/1.1
+    Request_Headers,      // Host: www.example.com User-Agent: Mozilla/5.0
+    Request_Body,        // man3rf mohim body
+    Request_Chunked,    // data chunked
+    Request_Finished   // request valid move on into response
+};
+
+class HttpRequest
 {
     private:
-        std::string                         _method;
-        std::string                         _path;
-        std::string                         _version;
-        std::map<std::string, std::string>  _headers;
-        std::string                         _body;
+        std::string                         method;        // GET, POST, DELETE
+        std::string                         path;          // /index.html
+        std::string                         version;       // HTTP/1.1
+        std::map<std::string, std::string>  headers;       // Host -> localhost
+        std::string                         body;          // POST data
+        RequestParseState                   state;
+        size_t                              contentLength; // From Header
+        std::string                         storage;       // Buffer for  lines \r\n
+        bool    parseRequestLine(std::string &line);
+        bool    parseHeaders(std::string &line);
+        void    parseBody(std::string &line);
+        void    parseChunked(std::string &line);
+
     public:
+
 };
 
 #endif
