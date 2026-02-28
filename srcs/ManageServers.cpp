de0a733455a7b1bf834a4d97e6a03f75942ab9b6
souel-bo <souel-bo@e2r8p4.1337.ma>
2026-02-28 00:44:17 +0000

    FIX : FDS override & leaks issue (FIXED)

--- srcs/ManageServers.cpp
+++ srcs/ManageServers.cpp
@@ -1,26 +1,26 @@
 #include "ManageServers.hpp"
 
 SocketManager::~SocketManager() {
-    // for (size_t i = 0; i < _listeningSockets.size(); ++i) {
-    //     ListeningSocket* sock = &_listeningSockets[i];
-    //     delete sock;
-    // }
-    // _listeningSockets.clear();
+    for (size_t i = 0; i < _listeningSockets.size(); ++i) {
+        ListeningSocket* sock = _listeningSockets[i];
+        delete sock;
+    }
+    _listeningSockets.clear();
     closeAllSockets();
 }
 
-void SocketManager::addSocket(const ListeningSocket& sock) {
+void SocketManager::addSocket( ListeningSocket *sock) {
     _listeningSockets.push_back(sock);
 }
 
-std::vector<ListeningSocket>& SocketManager::getSockets() {
+std::vector<ListeningSocket *> SocketManager::getSockets() {
     return _listeningSockets;
 }
 
 std::vector<int> SocketManager::getFds() const {
     std::vector<int> fds;
     for (size_t i = 0; i < _listeningSockets.size(); ++i) {
-        fds.push_back(_listeningSockets[i].getFd());
+        fds.push_back(_listeningSockets[i]->getFd());
     }
     return fds;
 }
@@ -33,13 +33,13 @@ void SocketManager::generateListeningSockets(const std::vector<ServerConfig>& se
         sock->bindSocket();
         sock->startListening(128);
         sock->setNonBlocking();
-        addSocket(*sock);
+        addSocket(sock);
     }
 }
 
 void SocketManager::closeAllSockets() {
     for (size_t i = 0; i < _listeningSockets.size(); ++i) {
-        close(_listeningSockets[i].getFd());
+        close(_listeningSockets[i]->getFd());
     }
     _listeningSockets.clear();
 }
